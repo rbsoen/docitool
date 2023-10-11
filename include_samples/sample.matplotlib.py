c50379667e9733b195cab2fc2e6cee96b302d713
Rodriguez Breil Soenoto <rbsoenoto@gmail.com>
2023-10-11 21:31:50 +0700

    more docs stuff

---
+++ include_samples/sample.matplotlib.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def abc():
+	return 1001
+
+np.random.seed(abc())
+plt.rcdefaults()
+fig, ax = plt.subplots()
+
+# Example data
+people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+
+ax.barh(y_pos, performance, xerr=error, align='center', color='blue', ecolor='black')
+ax.set_yticks(y_pos)
+ax.set_yticklabels(people)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Performance')
+ax.set_title('How fast do you want to go today?',{'fontname':'serif'})
